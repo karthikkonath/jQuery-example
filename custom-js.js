559c5f04532b7f7b76f1dcca4259d92277726653
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-09-23 17:44:48 -0500

    the good good

---
+++ custom-js.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // MAD Introduction to Javascript & jQuery
+    console.log("custom-js.js loaded");              
+    
+    // Add a click event to the submit-btn
+});
